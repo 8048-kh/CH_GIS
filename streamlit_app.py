6eb5e85139de07b2e464a07c92935360e61bd24a
8048-kh <8046khtotn@gmail.com>
2026-01-06 11:50:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+
+st.title("🎈 HI HI")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
+import streamlit as st
 import leafmap.foliumap as leafmap
 import pandas as pd
 import folium
